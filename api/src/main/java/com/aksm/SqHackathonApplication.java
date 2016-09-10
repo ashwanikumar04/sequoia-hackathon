5ac6f2a18ab557fcfd7ac3a5692508f78633fcbf
Ashwani Kumar <ashwanikumar04@gmail.com>
2016-09-10 16:22:45 +0530

    Added Base

---
+++ api/src/main/java/com/aksm/SqHackathonApplication.java
@@ -0,0 +1,12 @@
+package com.aksm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqHackathonApplication.class, args);
+	}
+}
